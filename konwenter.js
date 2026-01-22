0658927b0e75c2764bcdc2efaf6e4abf16052f95
bartekaugustyniak12-glitch <bartekaugustyniak12@gmail.com>
2026-01-22 11:58:02 +0100

    Delete konwenter.js

--- konwenter.js
+++
@@ -1,7 +0,0 @@
-function konwertuj() {
-    let metry = document.getElementById("liczba").value;
-    let kilometry = metry / 1000;
-
-    document.getElementById("wynik").innerText =
-        metry + " metrów = " + kilometry + " kilometrów";
-}

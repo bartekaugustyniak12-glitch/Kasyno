5b23e0c860733aa0bf47a98d9b5f05a46a6a93c3
bartekaugustyniak12-glitch <bartekaugustyniak12@gmail.com>
2026-01-29 07:48:56 +0100

    Add files via upload

--- Kasyno-main/skrypt_pulpit.js
+++ Kasyno-main/skrypt_pulpit.js
@@ -2,3 +2,30 @@
         document.getElementById("zegarek").innerHTML =
             new Date().toLocaleString("pl-PL");
     }, 1000);
+   const info=[
+    "Ośmiornice mają trzy serca i niebieską krew",
+
+"Banany są jagodami, a truskawki nie są",
+
+"W kosmosie nie da się płakać, bo łzy nie spływają",
+
+"Pszczoły potrafią rozpoznawać ludzkie twarze",
+
+"Serce wieloryba jest wielkości małego samochodu",
+
+"Ludzie dzielą około sześćdziesiąt procent DNA z bananem",
+
+"Rekiny istniały na Ziemi wcześniej niż drzewa",
+
+"Każdy ludzki język jest unikalny jak odcisk palca",
+
+"Ketchup był kiedyś sprzedawany jako lekarstwo",
+
+"Na Wenus jeden dzień trwa dłużej niż cały rok"]
+
+const dzien = new Date().getDate();
+const ciekawostka = dzien % info.length;
+
+document.getElementById("ciekawostka").innerText = info[ciekawostka];
+
+
